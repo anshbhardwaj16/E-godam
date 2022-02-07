89e846e58f7c02b937472a056c57a51bf29f3de1
anshbhardwaj16 <66911831+anshbhardwaj16@users.noreply.github.com>
2022-02-07 22:56:03 +0530

    Create registrion.php

---
+++ registrion.php
@@ -0,0 +1,29 @@
+5.1.1 Code for registration.php
+<?php
+ $name=$user=$pass1=$pass2=$email="";
+$conn=@mysqli_connect("localhost","root","root","emandi");
+if($_SERVER["REQUEST_METHOD"]=="POST")
+{
+$name=$_POST["name"];
+$user=$_POST["user"];
+$pass1=$_POST["pass1"];
+ $pass2=$_POST["pass2"];
+$email=$_POST["email"];
+ if($pass1==$pass2)
+ {
+ $query="insert into register values('".$name."','".$user."' 
+,'".$pass1."','".$pass2."','".$email."')";
+ $result=@mysqli_query($conn,$query);
+ if($result)
+ {
+ echo '<script type="text/javascript"> alert("USER REGISTERED 
+SUCCESFULLY") </script>';
+ }
+ }
+ else
+ {
+ echo '<script type="text/javascript"> alert("passwords did not match") 
+</script>';
+ }
+ }
+ ?>
